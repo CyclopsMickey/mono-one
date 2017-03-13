48e6d114644b2148e00dcb4d0ee20629c7546d34
jimabradford <jimabradford@gmail.com>
2017-03-13 08:24:07 -0400

    Excercise 1-3 and 1-4

---
+++ kr-1-4.c
@@ -0,0 +1,20 @@
+// print Celsius-Fahrenheit table for fahr = -17, 10, ..., 120
+
+#include <stdio.h>
+
+int main() {
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = -17; 	/* lower limit of temperature scale */
+	upper = 120;	/* upper limit */
+	step = 10;	/* step size */
+
+	celsius = lower;
+	while (fahr <= upper) {
+		fahr = ((celsius/5.0)*9.0) + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+	return 0;
+}
