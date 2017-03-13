2f25df3ed7f4ffc981a59e0b240ecfb102ed4bb9
Jim Bradford <jimabradford@gmail.com>
2017-03-13 21:33:26 +0000

    KR Exercise 1-8 Line Count

--- kr-1-8.c
+++ kr-1-8.c
@@ -1,9 +1,19 @@
 /* copy input to output; 2nd version */
 #include <stdio.h>
- 
-int main() 
+
+int main()
 {
-        double nc;
-        for (nc = 0; getchar() != EOF; ++nc);
-        printf("%.0f\n", nc);
+        //double nc;
+        //for (nc = 0; getchar() != EOF; ++nc);
+        //printf("%.0f\n", nc);
+
+        int c, n1;
+        n1 = 0;
+
+        while ((c = getchar()) != EOF)
+                if (c == '\n') {
+                        ++n1;
+                }
+        
+        printf("%d\n", n1);
 }
