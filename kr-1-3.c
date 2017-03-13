ffe48b30d1197f7fae1f42be4383160a0825f18d
jimabradford <jimabradford@gmail.com>
2017-03-13 08:52:26 -0400

    KR Exercise 1-3

--- kr-1-3.c
+++ kr-1-3.c
@@ -11,6 +11,7 @@ int main() {
 	step = 20;	/* step size */
 
 	fahr = lower;
+	printf("%3s %6s\n", "fahr", "celsius");
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%3.0f %6.1f\n", fahr, celsius);
