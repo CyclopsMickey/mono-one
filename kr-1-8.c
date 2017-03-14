41e58d710b354800fc90fed02cc9238dd7b07b18
jimabradford <jimabradford@gmail.com>
2017-03-14 09:32:15 -0400

    KR Exercise 1-8 Blank, Tab and New Line count

--- kr-1-8.c
+++ kr-1-8.c
@@ -7,13 +7,22 @@ int main()
         //for (nc = 0; getchar() != EOF; ++nc);
         //printf("%.0f\n", nc);
 
-        int c, n1;
+        int c, n1, t1, b1;
         n1 = 0;
+	t1 = 0;
+	b1 = 0;
 
-        while ((c = getchar()) != EOF)
+        while ((c = getchar()) != EOF) {
                 if (c == '\n') {
                         ++n1;
                 }
+                if (c == '\t') {
+                        ++t1;
+                }
+                if (c == 32) {
+                        ++b1;
+                }
+	}
         
-        printf("%d\n", n1);
+        printf("New Lines %d Tabs %d Blanks %d\n", n1, t1, b1);
 }
