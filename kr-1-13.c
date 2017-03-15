684fc89e89502db41fb9414368494592a3a7f681
jimabradford <jimabradford@gmail.com>
2017-03-15 10:14:42 -0400

    Print a histogram of the lengths of words
    Signed-off-by: jimabradford <jimabradford@gmail.com>

--- kr-1-13.c
+++ kr-1-13.c
@@ -1,26 +1,33 @@
 /* count digits, white space, others */
 #include <stdio.h>
 
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
 int main()
 {
-        int c, i, nwhite, nother;
-        int ndigit[10];
-        
-        nwhite = nother = 0;
-        for (i = 0; i < 10; ++i)
-                ndigit[i] = 0;
-        
-        while ((c = getchar()) != EOF)
-                if (c >= '0' && c <= '9')
-                        ++ndigit[c-'0'];
-                else if (c == ' ' || c == '\n' || c == '\t')
-                        ++nwhite;
-                else
-                        ++nother;
-        
-        printf("digits =");
-        for (i = 0; i < 10; ++i)
-                printf(" %d", ndigit[i]);
-        
-        printf(", white space = %d, other = %d\n", nwhite, nother);
+	int c, state, i, j, k, n;
+	int nwords[50];
+
+	state = OUT;
+	i = 0;
+	n = 1;
+	while ((c = getchar()) != EOF) {
+		//putchar(c);
+		if (c == ' ' || c == '\n' || c == '\t'){
+			state = OUT;
+			nwords[i] = n;
+			n = 1;
+                        ++i;
+			//putchar(10);
+		} else if (state == OUT) {
+			state = IN;
+		} else ++n;
+	}
+	for (j = 0; j < i; ++j) {
+                for (k = 0; k < nwords[j]; ++k)
+                        printf("-");
+                printf("\n");
+        }
+        printf("\n");
 }
