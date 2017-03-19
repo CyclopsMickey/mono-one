d223a86aaf4b86e52ee1c82f2e3e220526c95cae
Jim Bradford <jimabradford@gmail.com>
2017-03-19 14:02:27 +0000

    Counts the frequency of char in the input and displays as a histogram
    Signed-off-by: Jim Bradford <jimabradford@gmail.com>

--- kr-1-14.c
+++ kr-1-14.c
@@ -5,7 +5,8 @@
 #define OUT 0 /* outside a word */
 #define ASCII_COUNT 255
 
-// Test Data: This is some text for a test that exemplifies the exasperation of the miasma that unfolds in a gargantuan manner before the multitude
+/* Test Data: This is some text for a test that exemplifies the exasperation of 
+   the miasma that unfolds in a gargantuan manner before the multitude */
 
 int main()
 {
@@ -14,18 +15,18 @@ int main()
 
 	for (i = 0; i < ASCII_COUNT; ++i)
 		nwords[i] = 0;
-		
+
 	state = OUT;
 	i = 0;
 	n = 1;
 	while ((c = getchar()) != EOF) {
 		++nwords[c];
 	}
-	
-	for (j = 1; j < ASCII_COUNT; ++j) {
+
+	for (j = 30; j < ASCII_COUNT; ++j) {
 		if (nwords[j] > 0) {
-			printf("%2d ", j);
-                	//printf("%2d", nwords[j]);
+			printf("%3d %c ", j, j);
+			//printf("%c %d", nwords[j], nwords[j]);
                 	for (k = 0; k < nwords[j]; ++k)
                         	printf("-");
                 	printf("\n");
