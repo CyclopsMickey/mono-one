65be026a9e7545a7f4af254f3504fb1009edadc8
jimabradford <jimabradford@gmail.com>
2018-02-20 09:21:54 -0500

    Excercise kr-2.1.c

---
+++ kr-2.1.c
@@ -0,0 +1,38 @@
+/******************************************************************************/
+// Exercise 1-23. 
+// Write a program to remove all comments from a C program. Don't forget to 
+// handle quoted strings and character constants properly. C comments don't nest.
+/******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+#define BIN 1 /* inside a blank section */
+#define BOUT 0 /* outside a blank section */
+
+/******************************************************************************/
+int main()
+{
+	int c, n, state;
+	
+	state = BOUT;
+	while ((c = getchar()) != EOF) {
+		if (c == '/') {
+			if ((n = getchar()) == '*') {
+				state = BIN;
+			} else {
+				putchar(c);
+				putchar(n);
+			}
+		} else if (c == '*') {
+			if ((n = getchar()) == '/') {
+				state = BOUT;
+			} else {
+				putchar(c);
+				putchar(n);
+			}
+		} else
+			putchar(c);
+	}
+	
+	printf("\nDone\n");
+}
