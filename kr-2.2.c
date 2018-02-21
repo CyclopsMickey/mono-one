b204577553cdcf47dde5f783d0dca2998dc3b802
jimabradford <jimabradford@gmail.com>
2018-02-21 04:49:28 -0500

    update

--- kr-2.2.c
+++ kr-2.2.c
@@ -1,6 +1,10 @@
 /******************************************************************************/
 // Exercise 2-2. 
 // Write a loop equivalent to the for loop above without using && or ||.
+//
+//  for (i=0; i < lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+//  s[i] = c;
+//
 /******************************************************************************/
 #include <stdio.h>
 #include <string.h>
@@ -8,9 +12,12 @@
 /******************************************************************************/
 int main()
 {
-i int;
-	
-	
-for (i=0; i < lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
-s[i] = c;
+	int c, i, lim;
+	char s[10]; 
+		
+	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+
+	s[i] = '\0';
+	return i;
 }
